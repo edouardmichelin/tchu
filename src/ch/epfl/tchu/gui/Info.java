53a984754ab7c71f65cdd238f5ac536972ccadb3
edouardmichelin <edouard.michelin15@gmail.com>
2021-03-11 16:33:20 +0100

    Premier complétage de la classe Info et sa classe de test - tous les tests passent jusque ici

--- src/ch/epfl/tchu/gui/Info.java
+++ src/ch/epfl/tchu/gui/Info.java
@@ -5,6 +5,7 @@ import ch.epfl.tchu.game.Card;
 import ch.epfl.tchu.game.Route;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Permet de générer les textes décrivant le déroulement de la partie
@@ -26,17 +27,25 @@ public final class Info {
      * @return le nom (français) de la carte donnée, au singulier ssi la valeur absolue du second argument vaut 1
      */
     public static String cardName(Card card, int count) {
+        String cardName = String.format("%s_CARD", card.color().toString());
+        try {
+            return String.format("%s%s", (String) StringsFr.class.getDeclaredField(cardName).get(StringsFr.class), StringsFr.plural(count));
+        } catch (Exception ignored) { }
+
         return "";
     }
 
     /**
      * Retourne le message déclarant que les joueurs, dont les noms sont ceux donnés, ont terminé la partie ex æqo en ayant chacun remporté les points donnés
-     * @param playerNames
-     * @param points
+     * @param playerNames names of the player
+     * @param points amount of points
      * @return le message déclarant que les joueurs, dont les noms sont ceux donnés, ont terminé la partie ex æqo en ayant chacun remporté les points donnés
      */
     public static String draw(List<String> playerNames, int points) {
-        return "";
+        int lastIndex = playerNames.size() - 1;
+        String commas = String.join(", ", playerNames.subList(0, lastIndex));
+
+        return String.format(StringsFr.DRAW, String.format("%s et %s", commas, playerNames.get(lastIndex)), points);
     }
 
 
