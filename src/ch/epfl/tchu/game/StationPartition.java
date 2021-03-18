0d9414fcfeada126b6cd083e24e3749921ad3d9e
edouardmichelin <edouard.michelin15@gmail.com>
2021-03-18 20:33:50 +0100

    Completion de la classe StationPartition et ajout des tests

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -1,12 +1,8 @@
 package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
-import ch.epfl.tchu.gui.Info;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 
 /**
  * Partission de stations
@@ -30,25 +26,42 @@ public final class StationPartition implements StationConnectivity {
 
     public static final class Builder {
         private final int[] stations;
+        private final boolean[] representatives;
 
         private Builder() {
             this.stations = null;
+            this.representatives = null;
         }
 
         public Builder(int stationCount) {
             Preconditions.checkArgument(stationCount >= 0);
 
+            this.representatives = new boolean[stationCount];
             this.stations = new int[stationCount];
+            Arrays.setAll(this.stations, p -> p);
         }
 
         /**
          * Joint les sous-ensembles contenant les deux gares passées en argument, en «élisant» l'un des deux représentants comme représentant du sous-ensemble joint ; retourne le bâtisseur (this)
-         * @param s1
-         * @param s2
+         * @param s1 station 1
+         * @param s2 station 2
          * @return le bâtisseur
          */
         public Builder connect(Station s1, Station s2) {
-            this.stations[this.representative(s1)] = s2.id();
+            if (this.representatives[s2.id()])
+                this.stations[s1.id()] = s2.id();
+            else if (this.representatives[s1.id()])
+                this.stations[s2.id()] = s1.id();
+            else {
+                if (this.representative(s1.id()) == s1.id())
+                    this.representatives[s1.id()] = true;
+                else {
+                    this.stations[s2.id()] = this.representative(s1.id());
+                    return this;
+                }
+
+                connect(s1, s2);
+            }
 
             return this;
         }
@@ -58,16 +71,25 @@ public final class StationPartition implements StationConnectivity {
          * @return la partition aplatie des gares correspondant à la partition profonde en cours de construction par ce bâtisseur
          */
         public StationPartition build() {
-            return null;
+            int[] links = Arrays.stream(this.stations).map(this::representative).toArray();
+            return new StationPartition(links);
         }
 
-        private int representative(Station station) {
-            int representative = -1;
-            while (representative != station.id()) {
-                representative = this.stations[representative > -1 ? representative : station.id()];
-            }
+        private int representative(int station) {
+            if (this.representatives[station])
+                return station;
 
-            return representative;
+            int witness = -1;
+            int representative = station;
+
+            while (true) {
+                representative = this.stations[representative];
+
+                if (witness == representative)
+                    return representative;
+
+                witness = representative;
+            }
         }
     }
 }
---
+++ src/ch/epfl/tchu/game/StationPartitionTest.java
@@ -0,0 +1,66 @@
+package ch.epfl.tchu.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Title
+ *
+ * @author Edouard Michelin (314770)
+ * @author Julien Jordan (315429)
+ */
+public class StationPartitionTest {
+
+    private static final Station FRI = new Station(9, "Fribourg");
+    private static final Station GEN = new Station(10, "Genève");
+    private static final Station LAU = new Station(13, "Lausanne");
+    private static final Station LCF = new Station(14, "La Chaux-de-Fonds");
+    private static final Station LUC = new Station(16, "Lucerne");
+    private static final Station LUG = new Station(17, "Lugano");
+    private static final Station MAR = new Station(18, "Martigny");
+    private static final Station NEU = new Station(19, "Neuchâtel");
+    private static final Station YVE = new Station(31, "Yverdon");
+    private static final Station ZOU = new Station(32, "Zoug");
+    private static final Station ZUR = new Station(33, "Zürich");
+
+    @Test
+    void stationPartitionShouldWork() {
+        var size = 34;
+
+        var connectivity = new StationPartition.Builder(size)
+                .connect(GEN, LAU)
+                .connect(LAU, YVE)
+                .connect(LAU, NEU)
+                .connect(LAU, LCF)
+                .connect(ZUR, ZOU)
+                .connect(ZUR, LUG)
+                .build();
+
+        System.out.println(connectivity);
+
+        var t1 = connectivity.connected(GEN, LAU);
+        var t2 = connectivity.connected(LAU, YVE);
+        var t3 = connectivity.connected(ZUR, LUG);
+        var f1 = connectivity.connected(LAU, ZUR);
+        var f2 = connectivity.connected(LAU, ZOU);
+        var f3 = connectivity.connected(LAU, FRI);
+
+        assertTrue(t1);
+        assertTrue(t2);
+        assertTrue(t3);
+        assertFalse(f1);
+        assertFalse(f2);
+        assertFalse(f3);
+    }
+
+    @Test
+    void stationPartitionBuilderFailsOnNegativeInput() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            var test = new StationPartition.Builder(-1);
+        });
+    }
+
+
+}
