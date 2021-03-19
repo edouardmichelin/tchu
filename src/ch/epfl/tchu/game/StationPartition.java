b4e4fb3b9bec918b93c0037734a593b756acc028
edouardmichelin <42861545+edouardmichelin@users.noreply.github.com>
2021-03-19 08:04:50 +0100

    Feature/step4 class station partition
    * Presque fini la classe
    
    * Completion de la classe StationPartition et ajout des tests
    
    * Petite amélioration et ajout d'une assertion dans un test

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -2,8 +2,7 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * Partission de stations
@@ -22,30 +21,49 @@ public final class StationPartition implements StationConnectivity {
 
     @Override
     public boolean connected(Station s1, Station s2) {
-        return false;
+        return this.links[s1.id()] == this.links[s2.id()];
     }
 
     public static final class Builder {
-        private final List<Station> stations;
+        private final int[] stations;
+        private final boolean[] representatives;
 
         private Builder() {
             this.stations = null;
+            this.representatives = null;
         }
 
         public Builder(int stationCount) {
             Preconditions.checkArgument(stationCount >= 0);
 
-            this.stations = new ArrayList<>(stationCount);
+            this.representatives = new boolean[stationCount];
+            this.stations = new int[stationCount];
+            Arrays.setAll(this.stations, p -> p);
         }
 
         /**
          * Joint les sous-ensembles contenant les deux gares passées en argument, en «élisant» l'un des deux représentants comme représentant du sous-ensemble joint ; retourne le bâtisseur (this)
-         * @param s1
-         * @param s2
+         * @param s1 station 1
+         * @param s2 station 2
          * @return le bâtisseur
          */
         public Builder connect(Station s1, Station s2) {
-            return null;
+            if (this.representatives[s2.id()])
+                this.stations[s1.id()] = s2.id();
+            else if (this.representatives[s1.id()])
+                this.stations[s2.id()] = s1.id();
+            else {
+                if (this.representative(s1.id()) == s1.id())
+                    this.representatives[s1.id()] = true;
+                else {
+                    this.stations[s2.id()] = this.representative(s1.id());
+                    return this;
+                }
+
+                connect(s1, s2);
+            }
+
+            return this;
         }
 
         /**
@@ -53,7 +71,24 @@ public final class StationPartition implements StationConnectivity {
          * @return la partition aplatie des gares correspondant à la partition profonde en cours de construction par ce bâtisseur
          */
         public StationPartition build() {
-            return null;
+            return new StationPartition(Arrays.copyOf(this.stations, this.stations.length));
+        }
+
+        private int representative(int station) {
+            if (this.representatives[station])
+                return station;
+
+            int witness = -1;
+            int representative = station;
+
+            while (true) {
+                representative = this.stations[representative];
+
+                if (witness == representative)
+                    return representative;
+
+                witness = representative;
+            }
         }
     }
 }
