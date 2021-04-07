44c0c6b14aed94881056e933c5b6aff8e628fd0a
NervzZ <julien.jordan@epfl.ch>
2021-04-07 02:30:17 +0200

    added TestPlayer class as is

---
+++ src/ch/epfl/tchu/game/TestPlayer.java
@@ -0,0 +1,54 @@
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+
+import java.util.List;
+import java.util.Random;
+
+private static final class TestPlayer implements Player {
+    private static final int TURN_LIMIT = 1000;
+
+    private final Random rng;
+    // Toutes les routes de la carte
+    private final List<Route> allRoutes;
+
+    private int turnCounter;
+    private PlayerState ownState;
+    private PublicGameState gameState;
+
+    // Lorsque nextTurn retourne CLAIM_ROUTE
+    private Route routeToClaim;
+    private SortedBag<Card> initialClaimCards;
+
+    public TestPlayer(long randomSeed, List<Route> allRoutes) {
+        this.rng = new Random(randomSeed);
+        this.allRoutes = List.copyOf(allRoutes);
+        this.turnCounter = 0;
+    }
+
+    @Override
+    public void updateState(PublicGameState newState, PlayerState ownState) {
+        this.gameState = newState;
+        this.ownState = ownState;
+    }
+
+    @Override
+    public TurnKind nextTurn() {
+        turnCounter += 1;
+        if (turnCounter > TURN_LIMIT)
+            throw new Error("Trop de tours joués !");
+
+        // Détermine les routes dont ce joueur peut s'emparer
+        List<Route> claimableRoutes = /* ... */;
+        if (claimableRoutes.isEmpty()) {
+            return TurnKind.DRAW_CARDS;
+        } else {
+            int routeIndex = rng.nextInt(claimableRoutes.size());
+            Route route = claimableRoutes.get(routeIndex);
+            List<SortedBag<Card>> cards = ownState.possibleClaimCards(route);
+
+            routeToClaim = route;
+            initialClaimCards = cards.get(0);
+            return TurnKind.CLAIM_ROUTE;
+        }
+    }
+}
