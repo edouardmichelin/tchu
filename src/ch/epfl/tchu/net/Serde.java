d91b0a613065fe265727763aa26c3578df5f39a0
edouardmichelin <42861545+edouardmichelin@users.noreply.github.com>
2021-04-26 05:34:49 +0200

    Ajout de l'interface Serde (#27)

---
+++ src/ch/epfl/tchu/net/Serde.java
@@ -0,0 +1,116 @@
+package ch.epfl.tchu.net;
+
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Serializer-deserializer
+ *
+ * @param <T> type que le serde prend en charge
+ * @author Edouard Michelin (314770)
+ * @author Julien Jordan (315429)
+ */
+public interface Serde<T> {
+    /**
+     * Prend en arguments une fonction de sérialisation et une fonction de désérialisation, et retourne le serde
+     * correspondant
+     *
+     * @param serializer  la fonction de sérialisation
+     * @param deserialize la fonction de dé-sérialisation
+     * @param <E>         le type à (dé-)sérialiser
+     * @return un <code>Serde</code> capable de (dé-)sérialiser le type <code>E</code> passé en argument
+     */
+    static <E> Serde<E> of(Function<E, String> serializer, Function<String, E> deserialize) {
+        return new Serde<>() {
+            @Override
+            public String serialize(E object) {
+                return serializer.apply(object);
+            }
+
+            @Override
+            public E deserialize(String str) {
+                return deserialize.apply(str);
+            }
+        };
+    }
+
+    /**
+     * Prend en argument la liste de toutes les valeurs d'un ensemble de valeurs énuméré et retourne le <i>Serde</i>
+     * correspondant
+     *
+     * @param list la liste de toutes les valeurs d'un multiensemble énuméré
+     * @param <E>  le type de l'énumération
+     * @return le <code>Serde</code> correspondant
+     */
+    static <E> Serde<E> oneOf(List<E> list) {
+        return null;
+    }
+
+    /**
+     * Prend en argument un <i>Serde</i> et un caractère de séparation et retourne un serde capable de (dé-)
+     * sérialiser des listes de valeurs (dé-)sérialisées par le <i>Serde</i> donné
+     *
+     * @param serde     le <code>Serde</code>
+     * @param separator le charactère de séparation
+     * @param <E>       le type à (dé-)sérialiser
+     * @return le <code>Serde</code> correspondant
+     */
+    static <E extends Comparable<E>> Serde<List<E>> listOf(Serde<E> serde, CharSequence separator) {
+        return new Serde<>() {
+            @Override
+            public String serialize(List<E> object) {
+                return object.stream().map(serde::serialize).collect(Collectors.joining(separator));
+            }
+
+            @Override
+            public List<E> deserialize(String str) {
+                return Arrays.stream(str.split(Pattern.quote(separator.toString()), -1)).map(serde::deserialize).collect(Collectors.toList());
+            }
+        };
+    }
+
+
+    /**
+     * Prend en argument un <i>Serde</i> et un caractère de séparation et retourne un serde capable de (dé-)
+     * sérialiser des listes triées de valeurs (dé-)sérialisées par le <i>Serde</i> donné
+     *
+     * @param serde     le <code>Serde</code>
+     * @param separator le charactère de séparation
+     * @param <E>       le type à (dé-)sérialiser
+     * @return le <code>Serde</code> correspondant
+     */
+    static <E extends Comparable<E>> Serde<SortedBag<E>> bagOf(Serde<E> serde, CharSequence separator) {
+        return new Serde<>() {
+            @Override
+            public String serialize(SortedBag<E> object) {
+                return object.stream().map(serde::serialize).collect(Collectors.joining(separator));
+            }
+
+            @Override
+            public SortedBag<E> deserialize(String str) {
+                return SortedBag.of(Arrays.stream(str.split(Pattern.quote(separator.toString()), -1)).map(serde::deserialize).collect(Collectors.toList()));
+            }
+        };
+    }
+
+    /**
+     * Retourne <i>objet</i> transformé en une chaîne de caractères (sérialisation)
+     *
+     * @param object l'object à sérialiser
+     * @return la sérialisation de l'argument <code>object</code>
+     */
+    String serialize(T object);
+
+    /**
+     * Retourne l'argument <i>string</i> transformé en un objet (dé-sérialisation)
+     *
+     * @param str la chaîne de caractères représentant un objet de type <code>T</code> sérialisé
+     * @return l'argument <code>str</code> transformé en un objet (dé-sérialisation)
+     */
+    T deserialize(String str);
+}
