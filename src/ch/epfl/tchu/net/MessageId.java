dc11133012e76ec94160a2304398d3839f77aa08
NervzZ <56892303+NervzZ@users.noreply.github.com>
2021-04-26 05:34:33 +0200

    Ajout du type énuméré MessageId (#26)

---
+++ src/ch/epfl/tchu/net/MessageId.java
@@ -0,0 +1,21 @@
+package ch.epfl.tchu.net;
+
+/**
+ * Les types de messages que le serveur peut envoyer aux clients
+ *
+ * @author Edouard Michelin (314770)
+ * @author Julien Jordan (315429)
+ */
+public enum MessageId {
+    INIT_PLAYERS,
+    RECEIVE_INFO,
+    UPDATE_STATE,
+    SET_INITIAL_TICKETS,
+    CHOOSE_INITIAL_TICKETS,
+    NEXT_TURN,
+    CHOOSE_TICKETS,
+    DRAW_SLOT,
+    ROUTE,
+    CARDS,
+    CHOOSE_ADDITIONAL_CARDS;
+}
