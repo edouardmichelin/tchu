832dc2b3f8cab965038e8ebceb5a60d42a4d10ff
NervzZ <julien.jordan@epfl.ch>
2021-05-24 20:44:40 +0200

    advanced panel 2 and 3 some more

--- src/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -6,10 +6,12 @@ import ch.epfl.tchu.game.Card;
 import ch.epfl.tchu.game.Ticket;
 import ch.epfl.tchu.gui.ActionHandlers.DrawCardHandler;
 import ch.epfl.tchu.gui.ActionHandlers.DrawTicketsHandler;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -28,15 +30,23 @@ import java.util.List;
  */
 class DecksViewCreator {
     public static Node createHandView(ObservableGameState gameState) {
+
+        SortedBag<Card> hand = gameState.cards();
+
         ObservableList<Ticket> playerTickets = FXCollections.observableArrayList(gameState.playerTickets());
         ListView<Ticket> ticketsView = new ListView<Ticket>(playerTickets);
         ticketsView.setId("tickets");
 
-        HBox handCardsView = new HBox(createCardView(Card.BLACK));
+        HBox handCardsView = new HBox();
         handCardsView.setId("hand-pane");
+        for (Card card : Card.ALL) {
+            if (hand.contains(card)) {
+                handCardsView.getChildren().add(createCardView(card, hand.countOf(card)));
+            }
+        }
 
-        HBox playerHandPane = new HBox(handCardsView, ticketsView);
-        playerHandPane.getStylesheets().addAll(List.of("decks.css", "colors.css"));
+        HBox playerHandPane = new HBox(ticketsView, handCardsView);
+        playerHandPane.getStylesheets().addAll("decks.css", "colors.css");
 
         return playerHandPane;
     }
@@ -47,32 +57,46 @@ class DecksViewCreator {
             ObjectProperty<DrawCardHandler> drawCard
     ) {
         VBox faceUpCardsView = new VBox();
-        for(Card card : gameState.faceUpCards()) {
+        for (Card card : gameState.faceUpCards()) {
             faceUpCardsView.getChildren().add(createCardView(card));
         }
+
+        VBox decksPane = new VBox();
+        decksPane.setId("card-pane");
+        decksPane.getStylesheets().addAll("decks.css", "colors.css");
+        return null;
+    }
+
+    private static Node createDrawButton(String label, IntegerProperty percentage) {
+
+
+        Button button = new Button();
+        button.getStyleClass().add("gauged");
         return null;
     }
 
     private static Node createCardView(Card card, int quantity) {
         Preconditions.checkArgument(quantity > 0);
+        String color = card.color() == null ? "NEUTRAL" : card.color().toString();
 
-        Rectangle cardBorder = new Rectangle();
+
+        Rectangle cardBorder = new Rectangle(60, 90);
         cardBorder.getStyleClass().add("outside");
 
-        Rectangle cardColor = new Rectangle();
-        cardColor.getStyleClass().addAll(List.of("filled", "inside"));
+        Rectangle cardColor = new Rectangle(40, 70);
+        cardColor.getStyleClass().addAll("filled", "inside");
 
-        Rectangle trainImage = new Rectangle();
+        Rectangle trainImage = new Rectangle(40, 70);
         trainImage.getStyleClass().add("train-image");
 
         Text count = new Text();
         count.getStyleClass().add("count");
-        if(quantity != 1) {
-            count.setText(""+quantity);
+        if (quantity != 1) {
+            count.setText("" + quantity);
         }
 
         StackPane mainCardPane = new StackPane(cardBorder, cardColor, trainImage, count);
-        mainCardPane.getStyleClass().addAll(List.of(card.toString(), "card"));
+        mainCardPane.getStyleClass().addAll(color, "card");
 
         return mainCardPane;
     }
